410629bd5a3a98388e57018d9d2482bc8c0621f1
Ubuntu <ubuntu@ip-172-31-55-50.ec2.internal>
2023-03-24 01:04:25 +0000

    adding new file

---
+++ instruction.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+#OWNER: Goshen Nkanta
+#DATE: March 23, 2023
+#DESCRIPTION: Introduction into automation using bash shell scripting.This file will contain various linux command to test run bash scripting.
+
+echo "hello there, you are"
+
+whoami
+
+pwd
+
